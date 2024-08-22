3e7f9cd8e2f565ef35289dbd0e0ba61e6b8914a3
Erick Clinton <erickleite2009321@gmail.com>
2024-08-22 17:00:50 -0300

    feat: crud product

---
+++ api/v1/product/router.go
@@ -0,0 +1,23 @@
+package product
+
+import (
+	"eccomerce/internal/v1/product/repository"
+	"eccomerce/internal/v1/product/services"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
+	repo := repository.NewProductRepository(db)
+	service := services.NewProductService(repo)
+	handler := NewHandler(service)
+
+	v1 := r.Group("/api/v1/product")
+	{
+		v1.POST("/", handler.create)
+		v1.GET("/:id", handler.GetByID)
+		v1.PUT("/:id", handler.UpdateById)
+		v1.DELETE("/:id", handler.Delete)
+	}
+}
