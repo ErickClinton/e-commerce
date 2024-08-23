829c882eae9a94070606f67adb66da829f02a44e
Renan Dos Santos <renan.santosoliveira0@gmail.com>
2024-08-23 16:34:34 -0300

    fix: added enviroment variable loader and adjusted service call.

--- pkg/authentication/auth.go
+++ pkg/authentication/auth.go
@@ -1,14 +1,28 @@
 package authentication
 
 import (
-	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
-	tokenService := &TokenManager{secretKey: []byte(os.Getenv("SECRET_KEY"))}
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+	}
+	secretKey := os.Getenv("SECRET_KEY")
+	if secretKey == "" {
+		log.Fatal("SECRET_KEY não definida nas variáveis de ambiente")
+	}
+	tokenExpiry := 24 * time.Hour
+	println(secretKey)
+	println("TESTE MIDDLEWARE")
+	tokenService := NewTokenManager(secretKey, tokenExpiry)
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 
